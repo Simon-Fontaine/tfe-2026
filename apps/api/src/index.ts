409271c961ae8906b507d917215f12ff0b181458
Simon Fontaine <github@simonfontaine.com>
2025-12-22 11:03:10 +0100

    feat(api): add basic hono api

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,5 +1,13 @@
-import { loginSchema } from "@workspaces/shared";
+import { Hono } from "hono";
+import { logger } from "hono/logger";
 
-console.log(
-  loginSchema.safeParse({ email: "test@test.com", password: "password123" }),
-);
+const app = new Hono();
+
+app.use("*", logger());
+
+const routes = app.get("/", (c) => {
+  return c.json({ message: "Hello, World!" });
+});
+
+export default app;
+export type AppType = typeof routes;
